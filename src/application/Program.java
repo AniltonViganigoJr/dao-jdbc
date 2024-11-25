bbf0507c9ff1794885617a2cc073216af7e854e4
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-11-25 11:25:50 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -9,8 +11,10 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST #1: Seller - FindById ===");
 		Seller seller = sellerDao.findById(3);
@@ -29,5 +33,10 @@ public class Program {
 		for(Seller s : sellers) {
 			System.out.println(s.getName() + " | Department: " + s.getDepartment().getName() );
 		}
+		
+		System.out.println("\n=== TEST #4: Seller - Insert One ===");
+		seller = new Seller(null, "João Alfredo", "alfredo@gmail.com", sdf.parse("29/11/1953"), 1200.00, new Department(4,null));
+		sellerDao.insert(seller);
+		System.out.println("Inserted New Id: " + seller.getId());
 	}
 }
